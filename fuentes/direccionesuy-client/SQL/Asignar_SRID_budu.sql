0beb1d82ed9a003f8f923947f04aece58b0c89a6
Andrés  Pastorini <andres.pastorini@agesic.gub.uy>
2022-10-25 14:07:11 -0300

    delete document
    delete document

--- fuentes/direccionesuy-client/SQL/Asignar_SRID_budu.sql
+++
@@ -1,9 +0,0 @@
-select * from geometry_columns
-where f_table_schema like 'budu';
-
-SELECT UpdateGeometrySRID('budu', 'codigos_postales_vigentes','multipolygon',32721);
-SELECT UpdateGeometrySRID('budu', 'punto_notable','punto',32721);
--- SELECT UpdateGeometrySRID('budu', 'mv_puntos_direccion','p4326',4326);
-SELECT UpdateGeometrySRID('budu', 'intersec_localidad_departamento','intersec',32721);
-SELECT UpdateGeometrySRID('budu', 'portal_pk','punto',32721);
-SELECT UpdateGeometrySRID('budu', 'punto_notable','punto',32721);
--- fuentes/direccionesuy-client/SQL/update_tipo_vialidad.sql
+++
@@ -1,2 +0,0 @@
-insert into budu.tipo_vialidad (id, nombre) values (20, 'CALLE INTERNA') ON CONFLICT DO NOTHING;
-insert into budu.tipo_vialidad (id, nombre) values (21, 'PASAJE INTERNO') ON CONFLICT DO NOTHING;
